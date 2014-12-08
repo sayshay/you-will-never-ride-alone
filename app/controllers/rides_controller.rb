8df5a787f903908b026c3b5e034de411047b8a0f
alexander <apprentice@jiuhiuh.local>
2014-12-08 15:38:08 -0800

    adds rides controller with restful routes

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,25 @@
+class RidesController < ApplicationController
+
+  def index
+    @rides = Ride.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
