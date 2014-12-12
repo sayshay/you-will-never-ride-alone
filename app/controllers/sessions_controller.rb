19da9a2fece7405f8d43a7a7ae44de956e521418
alexander <alexandr.larionov88@gmail.com>
2014-12-11 16:30:00 -0800

    fixed signup/login redirection adds background image removed navbar margin bottom

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where(:name => params[:signin][:name]).first
-    if user && user.authenticate(params[:signin][:password])
-      session[:user_id] = user.id
-      flash[:notice] = "Signed in successfully."
-      redirect_to users_path
+
+    @user = User.where(:name => params[:signin][:name]).first
+    if @user && @user.authenticate(params[:signin][:password])
+      session[:user_id] = @user.id
+      redirect_to rides_path
     else
       flash[:error] = "Sorry."
       render :new
     end
   end
-  
+
   def destroy
     session.clear
-    redirect_to rides_path
+    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
@@ -25,16 +21,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        session[:user_id] = @user.id
+        redirect_to rides_path, notice: 'User was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
   end
 
   # PATCH/PUT /users/1
@@ -51,8 +43,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
